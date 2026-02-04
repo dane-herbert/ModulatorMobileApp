81979bd8bdc4f56a96fcd2530ec273c5ef6537fc
danesherbert <danesherbert@gmail.com>
2026-02-04 13:54:16 -0600

    Update UI and add Modulators page
    - Changed AppShell.xaml to a lighter theme with new FlyoutItems for "Home" and "Modulators".
    - Modified MainPage.xaml layout, updated image properties, and enhanced UI elements.
    - Updated ModulatorApp.csproj to remove an image reference and add new package dependencies.
    - Introduced Modulators.xaml for a new content page with a layout for logging doses.
    - Created Modulators.xaml.cs for the code-behind of the new Modulators page.

---
+++ ModulatorApp/Modulators.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ModulatorApp
+{
+    public partial class Modulators : ContentPage
+    {
+        int count = 0;
+
+        public Modulators()
+        {
+            InitializeComponent();
+        }
+    }
+}
