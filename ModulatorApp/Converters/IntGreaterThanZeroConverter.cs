cc7fb387c2f7ff79949ae8bae9c6c8df91e7bd6b
danesherbert <danesherbert@gmail.com>
2026-02-07 08:26:23 -0600

    Add IntGreaterThanZeroConverter for visibility control
    - Introduce a new value converter, `IntGreaterThanZeroConverter`, to check if an integer is greater than zero.
    - This converter is utilized in `MainPage.xaml` to manage the visibility of a `Frame` based on the count of `Cards`, with support for an "invert" parameter.

---
+++ ModulatorApp/Converters/IntGreaterThanZeroConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.Maui.Controls;
+
+namespace ModulatorApp.Converters
+{
+    public class IntGreaterThanZeroConverter : IValueConverter
+    {
+        // If parameter == "invert", invert the boolean result.
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            bool isGreater = false;
+            if (value is int i)
+                isGreater = i > 0;
+            else if (value is long l)
+                isGreater = l > 0;
+            else if (value != null && int.TryParse(value.ToString(), out var parsed))
+                isGreater = parsed > 0;
+
+            if (parameter is string s && s.Equals("invert", StringComparison.OrdinalIgnoreCase))
+                isGreater = !isGreater;
+
+            return isGreater;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) =>
+            throw new NotImplementedException();
+    }
+}
